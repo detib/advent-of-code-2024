2ee8e3dba9d128b36e5f0b27c3b69e79fa18639b
detib <detibaholli@gmail.com>
2025-01-03 23:58:54 +0100

    day 24

--- Solutions/Day24/Day24.cs
+++ Solutions/Day24/Day24.cs
@@ -92,5 +92,88 @@ internal class Day24Part1 : IPart1Challenge
 
         return decimalValue;
     }
+}
 
+internal class Day24Part2 : IPart2Challenge
+{
+    public DateTime Day => new(2024, 12, 24);
+    public string Name => "Crossed Wires";
+    public bool IsActive => false;
+    public string Part2Result => "ckj,dbp,fdv,kdf,rpp,z15,z23,z39";
+
+    // https://github.com/robhabraken/advent-of-code-2024/blob/main/solutions/24/part-2/Program.cs
+    public async Task ExecuteAsync()
+    {
+        var input = (await File.ReadAllTextAsync("./Day24/input.txt")).Split("\r\n\r\n");
+
+        var secondPart = input[1].Split("\r\n");
+
+        var wires = new SortedDictionary<string, Wire>();
+        var gates = new List<Gate>();
+
+        // read all wires and gates
+        foreach (var line in secondPart)
+        {
+            if (line.Contains("->"))
+            {
+                var elements = line.Split(' ');
+                AddWire(elements[0]);
+                AddWire(elements[2]);
+                AddWire(elements[4]);
+
+                gates.Add(new Gate(wires[elements[0]], wires[elements[2]], wires[elements[4]], elements[1]));
+            }
+        }
+
+        var suspiciousGates = new List<Gate>();
+        var outputWires = wires.Values.Select(w => w).Where(w => w.Name.StartsWith('z')).ToList();
+        foreach (var gate in gates)
+        {
+            // starting gates should be followed by OR if AND, and by AND if XOR, except for the first one
+            if ((gate.Inputs[0].Name.StartsWith('x') || gate.Inputs[1].Name.StartsWith('x')) &&
+                (gate.Inputs[0].Name.StartsWith('y') || gate.Inputs[1].Name.StartsWith('y')) &&
+                (!gate.Inputs[0].Name.Contains("00") && !gate.Inputs[1].Name.Contains("00")))
+                foreach (var secondGate in gates)
+                    if (gate.Output == secondGate.Inputs[0] || gate.Output == secondGate.Inputs[1])
+                        if ((gate.Op.Equals("AND") && secondGate.Op.Equals("AND")) ||
+                            (gate.Op.Equals("XOR") && secondGate.Op.Equals("OR")))
+                            suspiciousGates.Add(gate);
+
+            // gates in the middle should not have XOR operators
+            if (!gate.Inputs[0].Name.StartsWith('x') && !gate.Inputs[1].Name.StartsWith('x') &&
+                !gate.Inputs[0].Name.StartsWith('y') && !gate.Inputs[1].Name.StartsWith('y') &&
+                !gate.Output.Name.StartsWith('z') && gate.Op.Equals("XOR"))
+                suspiciousGates.Add(gate);
+
+            // gates at the end should always have XOR operators, except for the last one
+            if (outputWires.Contains(gate.Output) && !gate.Output.Name.Equals($"z{outputWires.Count - 1}") && !gate.Op.Equals("XOR"))
+                suspiciousGates.Add(gate);
+        }
+
+        var answer = string.Empty;
+        foreach (var sGate in suspiciousGates.OrderBy(x => x.Output.Name))
+            answer += $"{sGate.Output.Name},";
+
+        Console.WriteLine(answer[..^1]);
+
+        return;
+        
+        void AddWire(string wireName)
+        {
+            if (!wires.ContainsKey(wireName))
+                wires.Add(wireName, new Wire(wireName));
+        }
+    }
+}
+
+internal class Gate(Wire in1, Wire in2, Wire output, string op)
+{
+    public Wire[] Inputs = [in1, in2];
+    public Wire Output = output;
+    public string Op = op;
+}
+
+internal class Wire(string name)
+{
+    public string Name = name;
 }
